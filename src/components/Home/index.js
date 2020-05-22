cae68dfcf91df3a243abb0505f7754dff3ea426b
Papu Kumar <papuruth@gmail.com>
2020-05-22 12:40:37 +0530

    updated db data

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,63 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Axios from 'axios';
 
 export default class Home extends Component {
-    render() {
-        return (
-            <div>
-                Home Works
-            </div>
-        )
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    };
+  }
+
+  async componentDidMount() {
+    const { response, error } = await this.fetchItems();
+    if (response) {
+      this.setState({
+        items: response
+      });
+    } else {
+      console.log(error);
     }
+  }
+
+  fetchItems = async () => {
+    try {
+      const response = await Axios.get(
+        'http://my-json-server.typicode.com/papuruth/mern-assignment-1/items'
+      );
+      return { response: response.data };
+    } catch (error) {
+      return { error };
+    }
+  };
+  render() {
+    const { items } = this.state;
+    return (
+      <div>
+        <ul>
+          {items.length > 0 &&
+            items.map(
+              ({
+                id,
+                item_id,
+                item_name,
+                item_make,
+                item_model,
+                item_model_year,
+                item_price
+              }) => (
+                <li key={id}>
+                  <div>
+                    <p>
+                      <span>Car Id</span>
+                      <span>{item_id}</span>
+                    </p>
+                  </div>
+                </li>
+              )
+            )}
+        </ul>
+      </div>
+    );
+  }
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,26 +1,31 @@
 import Home from '../components/Home';
 import CreateItem from '../components/CreateItems';
 import ItemsDetails from '../components/ItemDetails';
+import NotFound from '../components/NotFound';
 
 export const routes = [
   {
     path: '/',
     exact: true,
+    key:"Home",
     component: Home
   },
   {
     path: '/create-items',
     exact: true,
+    key:"CreateItem",
     component: CreateItem
   },
   {
     path: '/item-details/:id',
     exact: true,
+    key:"ItemsDetails",
     component: ItemsDetails
   },
   {
     path: '*',
     exact: true,
-    component: Home
+    key:"NotFound",
+    component: NotFound
   }
 ];
